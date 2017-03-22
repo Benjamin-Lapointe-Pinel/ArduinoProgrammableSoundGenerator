fd88a333e97085c0e0149870f2ecdb84485a6491
Benjamin Lapointe-Pinel (superbiji) <benjamin.lapointe13@gmail.com>
2017-03-22 09:03:45 -0400

    V1

---
+++ SoundInterfaceDevice.cpp
@@ -0,0 +1,21 @@
+#include "SoundInterfaceDevice.h"
+
+/*int16_t seed = 0x5EED5EED;
+
+void set_noise_seed(int16_t s)
+{
+  seed = s;
+}
+
+int16_t get_noise()
+{
+  uint16_t r = seed >> 6;
+  r = (r & 0x01) ^ (r >> 1);
+  seed = (seed << 1) | r;
+  return r;
+}*/
+
+
+
+
+
---
+++ SoundInterfaceDevice.h
@@ -0,0 +1,10 @@
+#ifndef SOUND_INTERFACE_DEVICE_H
+#define SOUND_INTERFACE_DEVICE_H
+
+#include <stdint.h>
+#include "note.h"
+
+/*void set_noise_seed(int16_t);
+int16_t get_noise();*/
+
+#endif
